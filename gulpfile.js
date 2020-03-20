4e58822db2b36482b8c36e10be0ede578a144e01
Wiseking5378 <tonysigarev2000@gmail.com>
2020-03-20 13:34:51 +0500

    feat: add normalize, grid, interface, variables, style, header

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,5 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-
-gulp.task('hello', function(done){
-    console.log('Привет, мир!');
-    done();
-})
-
-
 // Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
